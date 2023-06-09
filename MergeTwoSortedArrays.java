1e99f68b43826292d70c89bce0c42a5813dbde48
Rohan Sachan <rohansachan08@gmail.com>
2023-06-09 23:33:14 +0530

    Added MergeTwoSortedArrays.java solution file

---
+++ MergeTwoSortedArrays.java
@@ -0,0 +1,19 @@
+public class MergeTwoSortedArrays {
+    public static void main(String[] args) {
+
+    }
+    public static int[] ninjaAndSortedArrays(int arr1[], int arr2[], int m, int n) {
+        int i = m-1;
+        int j = n-1;
+        int k = m+n-1;
+        while(j>=0){
+            if(i>=0 && arr1[i] > arr2[j]){
+                arr1[k--] = arr1[i--];
+            }
+            else{
+                arr1[k--] = arr2[j--];
+            }
+        }
+        return arr1;
+    }
+}
